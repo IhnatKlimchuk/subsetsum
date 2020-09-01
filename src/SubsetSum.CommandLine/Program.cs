92e8f62421f3f4da0f1423745445dadb404e291a
Ihnat Klimchuk <ihnat.klimchuk@gmail.com>
2020-09-01 02:16:42 -0400

    Add cli base

--- src/SubsetSum.CommandLine/Program.cs
+++ src/SubsetSum.CommandLine/Program.cs
@@ -1,12 +1,48 @@
-﻿using System;
+﻿using CommandLine;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace SubsetSum.CommandLine
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            await Parser.Default.ParseArguments<SubsetSumOptions>(args)
+                .WithParsedAsync(SolveSubsetSumAsync);
+        }
+
+        private static async Task SolveSubsetSumAsync(SubsetSumOptions options)
+        {
+            using ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+            {
+                if (options.IsVerbose)
+                {
+                    builder
+                        .AddConsole()
+                        .SetMinimumLevel(LogLevel.Debug);
+                }
+            });
+
+            var result = await SolveAsync(options.Set, options.Sum.Value, loggerFactory);
+
+            if (result == null)
+            {
+                Console.Error.WriteLine("No solution found.");
+            }
+            else
+            {
+                Console.Out.WriteLine($"Solution found: {string.Join(", ", result)}.");
+            }
+        }
+
+        private static Task<ICollection<uint>> SolveAsync(IEnumerable<uint> set, uint sum, ILoggerFactory loggerFactory)
+        {
+            ILogger logger = loggerFactory.CreateLogger("subsetsum");
+            logger.LogInformation("Emulating solving with no results.");
+            return Task.FromResult<ICollection<uint>>(null);
         }
     }
 }
---
+++ src/SubsetSum.CommandLine/SubsetSumOptions.cs
@@ -0,0 +1,17 @@
+﻿using CommandLine;
+using System.Collections.Generic;
+
+namespace SubsetSum.CommandLine
+{
+    public class SubsetSumOptions
+    {
+        [Option('v', "verbose", Default = false, Required = false, HelpText = "Prints all messages to standard output.")]
+        public bool IsVerbose { get; set; }
+
+        [Value(0, Required = true, HelpText = "Sum that is required to achieve.", MetaName = "Sum")]
+        public uint? Sum { get; set; }
+
+        [Value(1, Min = 1, Required = true, HelpText = "Set with all possible numbers.", MetaName = "Set")]
+        public IEnumerable<uint> Set { get; set; }
+    }
+}
