d5ac0c8a42b2a3cd1b3f09b78a1a6014f84326d3
Ihnat Klimchuk <ihnat.klimchuk@gmail.com>
2020-09-08 10:31:36 -0400

    Add experimental NumberArgument

---
+++ src/SubsetSum/Experimental/NumberArgument.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace SubsetSum.Experimental
+{
+    public sealed class NumberArgument
+    {
+        private const char Dot = '.';
+        private const char Minus = '-';
+        private const char Plus = '+';
+        private const char Zero = '0';
+        private const char Nine = '9';
+
+        public string Original { get; private set; }
+        public string IntegerPart { get; private set; }
+        public string FractionalPart { get; private set; }
+        public bool IsNegative { get; private set; }
+        public bool IsNeutral { get; private set; }
+
+        private NumberArgument(string original, string integerPart, string fractionalPart, bool isNegative, bool isNeutral)
+        {
+            Original = original;
+            IntegerPart = integerPart;
+            FractionalPart = fractionalPart;
+            IsNegative = isNegative;
+            IsNeutral = isNeutral;
+        }
+
+        public static NumberArgument Parse(string original)
+        {
+            var number = original.AsSpan();
+            
+            ReadOnlySpan<char> integerPart;
+            ReadOnlySpan<char> fractionalPart;
+            int splitIndex = number.IndexOf(Dot);
+            if (splitIndex >= 0)
+            {
+                integerPart = number.Slice(0, splitIndex);
+                fractionalPart = number.Slice(splitIndex + 1);
+            }
+            else
+            {
+                integerPart = number;
+                fractionalPart = ReadOnlySpan<char>.Empty;
+            }
+
+            bool isNegative = false;
+            var firstCharacter = integerPart[0];
+            if (firstCharacter == Minus)
+            {
+                isNegative = true;
+                integerPart = integerPart.Slice(1);
+            }
+            else if (firstCharacter == Plus)
+            {
+                integerPart = integerPart.Slice(1);
+            }
+
+            integerPart = integerPart.TrimStart(Zero);
+            fractionalPart = fractionalPart.TrimEnd(Zero);
+
+            bool isNeutral = true;
+            char unknownCharacter = default;
+            if (!IsOnlyDigits(integerPart, ref unknownCharacter, ref isNeutral) || !IsOnlyDigits(fractionalPart, ref unknownCharacter, ref isNeutral))
+            {
+                throw new ArgumentException($"Unexpected symbol '{unknownCharacter}' in {number.ToString()}");
+            }
+
+            return new NumberArgument(original, integerPart.ToString(), fractionalPart.ToString(), isNegative, isNeutral);
+        }
+
+        private static bool IsOnlyDigits(ReadOnlySpan<char> number, ref char unknownCharacter, ref bool isNeutral)
+        {
+            foreach (var character in number)
+            {
+                if (character < Zero || character > Nine)
+                {
+                    unknownCharacter = character;
+                    return false;
+                }
+                isNeutral &= character == Zero;
+            }
+            return true;
+        }
+    }
+}
---
+++ src/SubsetSum/Experimental/SubsetSumSolver.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SubsetSum.Experimental
+{
+    public class SubsetSumSolver : ISubsetSumSolver<string>
+    {
+        private readonly ILogger logger;
+        private readonly AlgorithmOptions options;
+        private readonly CultureInfo cultureInfo;
+
+        public SubsetSumSolver(
+            AlgorithmOptions options,
+            ILogger<UInt32RecursionSubsetSumSolver> logger)
+        {
+            this.options = options ?? throw new ArgumentNullException(nameof(options));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this.cultureInfo = CultureInfo.InvariantCulture;
+        }
+
+        public Task<IReadOnlyCollection<string>> SolveAsync(string sum, string[] set, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
